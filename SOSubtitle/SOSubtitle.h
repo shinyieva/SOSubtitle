2e929dbe2e376881250463db9814d6f32c87db44
Sergio Ortega <sortega@aurigae.com>
2015-01-07 08:03:05 +0100

    Initial Commit

---
+++ SOSubtitle/SOSubtitle.h
@@ -0,0 +1,13 @@
+//
+//  SOSubtitle.h
+//  SOSubtitle
+//
+//  Created by so30 on 07/01/15.
+//  Copyright (c) 2015 Sergio Ortega. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SOSubtitle : NSObject
+
+@end
---
+++ SOSubtitle/SOSubtitle.m
@@ -0,0 +1,13 @@
+//
+//  SOSubtitle.m
+//  SOSubtitle
+//
+//  Created by so30 on 07/01/15.
+//  Copyright (c) 2015 Sergio Ortega. All rights reserved.
+//
+
+#import "SOSubtitle.h"
+
+@implementation SOSubtitle
+
+@end
