8804923a2bd67a703a829a102395c98bb1eb916f
Sergio Ortega <sortega@aurigae.com>
2015-01-23 12:55:21 +0100

    EDIT fix wrong font when no html string

--- SOSubtitle/NSString+HTML.m
+++ SOSubtitle/NSString+HTML.m
@@ -11,60 +11,70 @@
 @implementation NSString (HTML)
 
 - (NSAttributedString *)HTMLString {
-
+    
     static const NSUInteger kDefaultFontSize = 20;
     static NSString * kDefaultFontFamily = @"HelveticaNeue";
-
+    
     NSString *string = [self copy];
     
-    NSMutableAttributedString *HTMLString;
+    if ([[string substringToIndex:1] isEqualToString:@"\n"]) {
+        string = [string substringFromIndex:1];
+    }
     
+    NSMutableAttributedString *HTMLString;
+    NSRange HTMLStringRange;
     if ([string rangeOfString:@"<[^>]+>" options:NSRegularExpressionSearch].location != NSNotFound) {
         NSDictionary *options = @{NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType};
         HTMLString =  [[NSMutableAttributedString alloc] initWithData:[string dataUsingEncoding:NSUTF16StringEncoding]
                                                               options:options
                                                    documentAttributes:nil
                                                                 error:NULL];
+        HTMLStringRange = NSMakeRange(0, [HTMLString.string length]);
+        
+        //Edit font size
+        [HTMLString beginEditing];
+        [HTMLString enumerateAttribute:NSFontAttributeName
+                               inRange:HTMLStringRange
+                               options:0
+                            usingBlock:^(id value, NSRange range, BOOL *stop) {
+                                if (value) {
+                                    UIFont *oldFont = (UIFont *)value;
+                                    NSString *fontName = kDefaultFontFamily;
+                                    if ([oldFont.fontName rangeOfString:@"Italic"].location != NSNotFound) {
+                                        fontName = [fontName stringByAppendingString:@"-Italic"];
+                                    } else if ([oldFont.fontName rangeOfString:@"Bold"].location != NSNotFound) {
+                                        fontName = [fontName stringByAppendingString:@"-Bold"];
+                                    }
+                                    UIFont *newFont = [UIFont fontWithName:fontName size:kDefaultFontSize];
+                                    [HTMLString removeAttribute:NSFontAttributeName range:range];
+                                    [HTMLString addAttribute:NSFontAttributeName value:newFont range:range];
+                                }
+                            }];
+        [HTMLString endEditing];
     }
+    
+    
     
     if (!HTMLString) {
         HTMLString = [[NSMutableAttributedString alloc] initWithString:string
-                                                            attributes:nil];
+                                                            attributes:@{
+                                                                         NSFontAttributeName: [UIFont fontWithName:kDefaultFontFamily
+                                                                                                              size:kDefaultFontSize]
+                                                                         }];
+        HTMLStringRange = NSMakeRange(0, [HTMLString.string length]);
     }
-    
-    NSRange HTMLStringRange = NSMakeRange(0, [HTMLString.string length]);
-    
-    //Edit font size
-    [HTMLString beginEditing];
-    [HTMLString enumerateAttribute:NSFontAttributeName
-                           inRange:HTMLStringRange
-                           options:0
-                        usingBlock:^(id value, NSRange range, BOOL *stop) {
-                            if (value) {
-                                UIFont *oldFont = (UIFont *)value;
-                                NSString *fontName = kDefaultFontFamily;
-                                if ([oldFont.fontName rangeOfString:@"Italic"].location != NSNotFound) {
-                                    fontName = [fontName stringByAppendingString:@"-Italic"];
-                                } else if ([oldFont.fontName rangeOfString:@"Bold"].location != NSNotFound) {
-                                    fontName = [fontName stringByAppendingString:@"-Bold"];
-                                }
-                                UIFont *newFont = [UIFont fontWithName:fontName size:kDefaultFontSize];
-                                [HTMLString removeAttribute:NSFontAttributeName range:range];
-                                [HTMLString addAttribute:NSFontAttributeName value:newFont range:range];
-                            }
-                        }];
-    [HTMLString endEditing];
     
     NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
     paragraphStyle.alignment = NSTextAlignmentCenter;
-    paragraphStyle.lineSpacing = kDefaultFontSize/2;
     
     //Add color and paragraph style
     [HTMLString addAttributes:@{
                                 NSParagraphStyleAttributeName: paragraphStyle,
-                                NSForegroundColorAttributeName: [UIColor whiteColor],
+                                NSForegroundColorAttributeName: [UIColor whiteColor]
                                 }
                         range:HTMLStringRange];
+    
+    
     
     return HTMLString;
 }
